f8217ddc1d31c71caa1493afc7049641cdbada00
Zygmunt Buszyk <stozzek@gmail.com>
2018-10-19 11:11:40 +0200

    huge push with production setup

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ app.use(cors());
 const posts = require('./routes/api/post');
 app.use('/api/posts', posts);
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // its true when deploying to a hosting service
+  //Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  //Handle single page app
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html')); //refering to 'any route at all'
+}
 //Setting up a server
 const port = process.env.PORT || 5000;
 
